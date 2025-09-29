f140ede45f4e013e263a7e5ef7dfcd1f2e894389
QuocKhanh2904 <khanhdoan849@gmail.com>
2025-09-29 07:45:04 +0700

    commit3

--- quanlidoan/home/urls.py
+++ quanlidoan/home/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('topic/', views.topic, name='topic'),
+    path('topic/register_topic/', views.register_topic, name='register_topic'),
     path('report_progress/', views.report_progress, name='report_progress'),
+    path('report_progress/report_detail/', views.report_detail, name='report_detail'),
 ]
